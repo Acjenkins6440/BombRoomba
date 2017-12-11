e9dba51ecfb070a89c360880e75bf53081461347
Alexander Jenkins <acjenkins6440@gmail.com>
2017-12-11 10:18:18 -0800

    Completed project on codepen, ported to Atom

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var box = '';
 var bombs = [];
+var flags = [];
 var numBombs = 10;
-var clickTile = {'border':'1px dotted black','height':'26px', 'width':'26px'};
-var bombpic = 'url("https://d30y9cdsu7xlg0.cloudfront.net/png/54644-200.png")';
+var clickTile = {'border':'1px dotted black','height':'28px', 'width':'28px'};
 var ur = 0
 var u = 0
 var ul = 0
@@ -11,6 +11,8 @@ var r = 0
 var lowr = 0
 var low = 0
 var lowl = 0
+var theTimer;
+var timer = parseInt($(".timer").text());
 function box(){
   return "<div class='box'></div>"
 }
@@ -25,6 +27,10 @@ $(document).ready(function(){
   gridSetup();
   $('.board').hide();
 });
+function setTimer(){
+  $(".timer").text(parseInt($(".timer").text())+1);
+}
+//setTimer utilized by var: theTimer
 function secondHandlerBinding(){
    $("#X").click(function(){
     confirm('Are you sure you want to quit?');
@@ -33,16 +39,16 @@ function secondHandlerBinding(){
   $("#X").on('click');
   $("#X").click(function(){
     if (confirm('Are you sure you want to quit?') == true){
-      $('.container').children().remove();
-      $('.board').hide();
-      $('.selector').show();}
+      resetBoard();}
   });
   $("#i").off('click');
   $("#i").click(function(){
     alert('Select Box: Left Click. Flag Box: Ctrl + Left Click');
-    console.log($('.small').attr('class'));
   });
+  $(".face").off('click');
+  $(".face").on('click');
   $(".container").children().mousedown(function(ev){
+    if($(this).hasClass('gameOver')){return;}
     if($(this).hasClass('box')){
       if ($(this).hasClass('clicked')){}
       else if (ev.ctrlKey){}
@@ -50,23 +56,41 @@ function secondHandlerBinding(){
         $(this).css('border', '4px inset grey');}
       }});
   $('.container').children().mouseup(function(ev){
+    if($(this).hasClass('gameOver')){return;}
+    var indy = parseInt($(this).attr('id'));
+    if (parseInt($(".timer").text())===0) {
+      $(".timer").text(1);
+      theTimer = setInterval(function(){setTimer()}, 1000);
+    }
     if ($(this).hasClass('box') && ev.ctrlKey && !($(this).hasClass('clicked'))){
+
       if ($(this).hasClass('flag')){
         $(this).removeClass('flag');
+        flags.splice(flags.indexOf(indy),1);
       }
       else{
+          flags.push(indy);
           $(this).addClass('flag');
+          if (bombs.length == flags.length){
+            if (bombs.sort().toString() == flags.sort().toString()){ youWon();}
+          }
         }
+      $(".score").text(numBombs - flags.length);
       }
 
     else if ($(this).hasClass('bmb')){
-      $(this).css('background-image', bombpic);
+      $(this).html("<i class='fa-bomb fa fa-lg' aria-hidden='true'></i>");
       $(this).addClass('clicked');
       $(this).css(clickTile);
+      youLose();
     }
     else{
+      if ($(this).hasClass('flag')){
+        flags.splice(flags.indexOf(indy),1);
+        $(".score").text(parseInt($(".score").text())+1);
+        $(this).removeClass('flag');
+      }
       $(this).css(clickTile);
-      $(this).removeClass('flag');
       $(this).addClass('clicked');
       if ($(this).attr('value') != '0'){
         $(this).text($(this).attr("value"));
@@ -78,29 +102,32 @@ function secondHandlerBinding(){
     }
   });
 }
+function little(){
+
+}
 function bindClickHandlers(){
   $("#boom").click(function(ev){
     console.log(ev.ctrlKey);
   });
   $(".small").click(function(){
+    $(".score").text(10);
     gridSetup(9,9,0);
     numBombs = 10;
     bombSetup(9*9);
     $(".face").css('flex','2')});
   $(".medium").click(function(){
+    $(".score").text(40);
     gridSetup(16,16,1);
     numBombs = 40;
     bombSetup(16*16);
     $(".face").css('flex','5');
     });
   $(".large").click(function(){
+    $(".score").text(99);
     gridSetup(16,30,2);
     numBombs = 99;
     bombSetup(16*30);
     $(".face").css('flex','10');});
-  $(".size").children().mousedown(function(){
-    $(this).css('border', '4px inset grey');
-  });
   $(".size").children().mouseup(function(){
     $(".selector").hide();
   });
@@ -113,20 +140,20 @@ function gridSetup(y,x,z){
   $('.board').show();
   if (z == 0){
     $('.container').animate(setGrid(252,260),0);
-    $('.board').animate(setGrid(252,300),0);
-    $('.scorebar').animate(setGrid(252,35),0);
+    $('.board').animate(setGrid(252,310),0);
+    $('.scorebar').animate(setGrid(252,36),0);
     $('.navbar').animate(setGrid(249,18),0);
   }
   else if (z == 1){
     $('.container').animate(setGrid(448,450),0);
-    $('.board').animate(setGrid(448,494),0);
-    $('.scorebar').animate(setGrid(448,34),0);
+    $('.board').animate(setGrid(448,506),0);
+    $('.scorebar').animate(setGrid(448,36),0);
     $('.navbar').animate(setGrid(445,18),0);
   }
   else{
     $('.container').animate(setGrid(840,450),0);
-    $('.board').animate(setGrid(840,494),0);
-    $('.scorebar').animate(setGrid(840,34),0);
+    $('.board').animate(setGrid(840,506),0);
+    $('.scorebar').animate(setGrid(840,36),0);
     $('.navbar').animate(setGrid(837,18),0);
   }
 
@@ -152,7 +179,7 @@ function bombSetup(area){
   /*first setup array*/
   while (bombs.length < numBombs){
     x = Math.floor(Math.random() * area)
-    if (bombs.includes(x) || x === 0){}
+    if (bombs.indexOf(x) != -1 || x === 0){}
     else{
       bombs.push(x);
     }
@@ -161,6 +188,7 @@ function bombSetup(area){
   bombs.forEach(function(x){
     $("#"+x).removeClass('blank');
     $('#'+x).addClass('bmb');
+
   });
   setNums();
   /*positions are different by 27.5px
@@ -262,6 +290,35 @@ function surrounding(boom){
       low = lowl = lowr = 0;
     }
   }
+function youWon(){
+  alert('Aww yeah you did it! \nClick the face to restart!');
+  clearInterval(theTimer);
+  $(".container").children().addClass('gameOver');
+  $(".face").click(resetBoard);
+}
+function youLose(){
+  $("#face").attr("class", 'fa fa-frown-o fa-2x');
+  //make sure they can see the end screen and click something else to reset
+  for (i =0; i<bombs.length;i++){
+    $("#"+bombs[i]).html("<i class='fa-bomb fa fa-lg' aria-hidden='true'></i>");
+      $("#"+bombs[i]).addClass('clicked');
+      $("#"+bombs[i]).css(clickTile);
+  }
+  alert('Oh no, you lost!\nClick the face to try again!');
+  clearInterval(theTimer);
+  $(".container").children().addClass('gameOver');
+  $(".face").click(resetBoard);
+}
+function resetBoard(){
+  clearInterval(theTimer);
+  flags = [];
+  $(".timer").text('0');
+  $('.container').children().remove();
+  $('.board').hide();
+  $('.selector').show();
+  $("#face").attr("class", 'fa fa-smile-o fa-2x');
+  $(".container").children().removeClass('gameOver');
+}
 function resetAndAddNums(){
   var squareNums = [ul,u,ur,r,l,lowl,low,lowr];
   squareNums.forEach(function(b){
